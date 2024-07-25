b999aecc9aca09a99f930c23b6ff428d17cb9f47
James Carney <jmcarneyiii@gmail.com>
2024-07-25 00:16:34 -0400

    extend a11y, import/ts, import/recommended, set up resolver, add import rules in eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -5,7 +7,18 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/typescript',
   ],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['~', path.resolve(__dirname, './src')]],
+        extensions: ['.js', '.jsx', '.ts', '.d.ts', '.tsx'],
+      },
+    },
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
@@ -15,5 +28,23 @@ module.exports = {
       { allowConstantExport: true },
     ],
     semi: ['warn'],
+    quotes: ['warn', 'single'],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'external',
+          'builtin',
+          'internal',
+          'sibling',
+          'parent',
+          'index',
+        ],
+      },
+    ],
+    'import/newline-after-import': [
+      'error',
+      { count: 2, exactCount: true, considerComments: true },
+    ],
   },
 };
