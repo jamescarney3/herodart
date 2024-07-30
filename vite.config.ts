127c4a57d1603ae5e8f9fb242a55f8d58c7003d3
James Carney <jmcarneyiii@gmail.com>
2024-07-30 02:53:46 -0400

    update vite config to use sass & tailwind funcs plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+// https://github.com/jquense/sass-tailwind-functions
+import sassTailwindFunctions from 'sass-tailwind-functions/modern';
 
 
 // https://vitejs.dev/config/
@@ -14,5 +16,17 @@ export default defineConfig({
   },
   test: {
     environment: 'jsdom',
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // inject tailwind funcs (theme & screen, plus a util escape func 'e')
+        // into sass scope, using the project tailwind config via the installed
+        // tailwind package so that it can resolve any theme extensions or
+        // overrides; otherwise these wouldn't be available since sass code is
+        // preprocessed
+        plugins: [sassTailwindFunctions],
+      },
+    },
   },
 });
