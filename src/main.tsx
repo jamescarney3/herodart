ea6ba79eccd836bc7ee07842701b65af84112705
James Carney <jmcarneyiii@gmail.com>
2024-08-01 00:35:30 -0400

    resolve eslint errors

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from '~/app.tsx';
 import '~/index.scss';
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 /// <reference types="vitest" />
+import path from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-// https://github.com/jquense/sass-tailwind-functions
 import sassTailwindFunctions from 'sass-tailwind-functions/modern';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
