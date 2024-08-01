51e8dc85fda6c157efb3ddc4487f5fd7109f8bd0
James Carney <jmcarneyiii@gmail.com>
2024-08-01 00:35:30 -0400

    resolve eslint errors

--- test/app.test.tsx
+++ test/app.test.tsx
@@ -3,7 +3,6 @@ import { render } from '@testing-library/react';
 
 import App from '~/app';
 
-
 describe('main app container', () => {
   it('renders without crashing', () => {
     const { container } = render(<App />);
