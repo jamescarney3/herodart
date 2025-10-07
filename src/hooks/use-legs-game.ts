52bf2a7b008de0b25a2d12944a6ccb5b9f914ca8
James Carney <jmcarneyiii@gmail.com>
2025-10-07 13:50:29 -0400

    HD-8 add legs game hook and tests

--- src/hooks/use-legs-game.ts
+++ src/hooks/use-legs-game.ts
@@ -1,28 +1,22 @@
-// import { useEffect, useState, useRef } from 'react';
-//
-// import LegsGame from '~/lib/legs/legs-game';
-//
-// const useLegsGame = () => {
-//   console.log('running hook');
-//   const [, forceUpdate] = useState(false);
-//   const [foo, setFoo] = useState(1);
-//   const barRef = useRef(1);
-//   const setBar = (val) => {
-//     barRef.current = val;
-//     forceUpdate(prev => !prev);
-//   };
-//
-//   const gameRef = useRef(null);
-//
-//   useEffect(() => {
-//     if (!gameRef.current) {
-//       const observe = () => forceUpdate(prev => !prev);
-//       gameRef.current = LegsGame.create({ id: 'main', observe });
-//       observe();
-//     }
-//   }, [gameRef]);
-//
-//   return { foo, setFoo, bar: barRef.current, setBar, game: gameRef.current || {} };
-// };
-//
-// export default useLegsGame;
+import { useEffect, useState, useRef } from 'react';
+
+import Observer from '~/lib/v2/observer';
+import LegsGame from '~/lib/legs/legs-game';
+
+const useLegsGame = () => {
+  const [, forceUpdate] = useState(false);
+  const gameRef = useRef(null);
+
+  useEffect(() => {
+    // id probably becomes a uuid or deserialized from some arbitrary kind of storage eventually
+    const id = 'main';
+    gameRef.current = gameRef.current ?? LegsGame.create({ id });
+    Observer.subscribe(forceUpdate);
+    forceUpdate();
+  }, [forceUpdate, gameRef]);
+
+
+  return { game: gameRef.current || {} };
+};
+
+export default useLegsGame;
