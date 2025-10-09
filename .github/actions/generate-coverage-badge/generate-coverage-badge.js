6ae53f4c7a78d6a9bd434e6b3d78c03840fedbd7
James Carney <jmcarneyiii@gmail.com>
2025-10-09 13:14:05 -0400

    HD-10 one last refactor to coverage badge workflow

--- .github/actions/generate-coverage-badge/generate-coverage-badge.js
+++ .github/actions/generate-coverage-badge/generate-coverage-badge.js
@@ -1,37 +1,31 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
 import * as core from '@actions/core';
-import * as github from '@actions/github';
 import { badgen } from 'badgen';
 
-/*
-  - use core to get summary input
-  - use path to resolve summary input path
-  - use fs to read the summary and JSON.parse to deserialize
-  - get the total lines covered from summary data
-  - persist percentage with core.setOutput *OR* write to file *OR* just make the badge in here
-*/
-
 const main = async () => {
+  // given a summary from the action config and workflow, read the json coverage
+  // summary file, deserialize, and get total line coverage percentage
   const summary = core.getInput('summary');
   const summaryPath = path.resolve(summary);
   const summaryContent = await fs.readFile(summaryPath, { encoding: 'utf8' });
   const coverage = JSON.parse(summaryContent);
-
   const lines = coverage.total.lines.pct;
 
+  // choose a color based on coverage thresholds
   let statusColor = 'red';
   if (lines >= 70) statusColor = 'orange'
   if (lines >= 80) statusColor = 'yellow'
   if (lines >= 90) statusColor = 'green'
 
+  // generate badge svg file with badgen and write it to the current working dir
   const svgString = badgen({
     label: 'Coverage',
     status: `${lines}%`,
     color: statusColor,
   });
-
   await fs.writeFile('./coverage.svg', svgString);
 };
 
+// invoke the function
 main().catch((err) => core.setFailed(`Action failed with error ${err}`));
