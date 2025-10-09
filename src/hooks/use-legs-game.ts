955b8bcbd03cacac329d1286d978c59da59b55c5
James Carney <jmcarneyiii@gmail.com>
2025-10-08 23:46:04 -0400

    HD-11 cleanup rest of application

--- src/hooks/use-legs-game.ts
+++ src/hooks/use-legs-game.ts
@@ -4,7 +4,7 @@ import Observer from '~/lib/v2/observer';
 import LegsGame from '~/lib/legs/legs-game';
 
 const useLegsGame = () => {
-  const [, forceUpdate] = useState<unknown>(false);
+  const [, forceUpdate] = useState<unknown>(new Object());
   const gameRef = useRef<LegsGame | null>(null);
 
   useEffect(() => {
@@ -13,14 +13,15 @@ const useLegsGame = () => {
     // compiler sees create invoked as *always* returning a Model instance even though it never
     // actually can - need to find a way to type this at the model level so it can know what its
     // actually getting when that method gets called
-    gameRef.current = gameRef.current ?? <LegsGame>LegsGame.create({ id });
-    Observer.subscribe(forceUpdate);
-    // @ts-expect-error I know this is an anti-pattern
-    forceUpdate();
+    gameRef.current ??= <LegsGame>LegsGame.create({ id });
+    // should this be a game serialization? important thing is that the new obj is referentially
+    // separate from the previous state value
+    Observer.subscribe(() => forceUpdate(() => new Object()));
+    forceUpdate(() => new Object());
   }, [forceUpdate, gameRef]);
 
 
-  return { game: gameRef.current || {} };
+  return { game: gameRef.current };
 };
 
 export default useLegsGame;
