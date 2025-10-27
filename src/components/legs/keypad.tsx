d4ffe73fe51124c23f4b37c43be7c25f2b78c355
James Carney <jmcarneyiii@gmail.com>
2025-10-27 12:51:40 -0400

    HD-14 add legs keypad tests and refactor

--- src/components/legs/keypad.tsx
+++ src/components/legs/keypad.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useRef } from 'react';
-import type { ChangeEvent, SyntheticEvent, ChangeEventHandler, MouseEventHandler } from 'react';
-import { formatEvalString, MULT, PLUS, MATH_MULT, MATH_PLUS } from '~/lib/utils';
+import type { ChangeEvent, SyntheticEvent, ChangeEventHandler } from 'react';
+import { formatEvalString, MULT, PLUS, MULT_MATH, PLUS_MATH } from '~/lib/utils';
+
+const operators: Array<string | undefined> = [MULT_MATH, PLUS_MATH];
 
 interface KeypadProps {
   onChange: ChangeEventHandler<HTMLInputElement>;
-  onSubmit: MouseEventHandler<HTMLButtonElement>;
+  onSubmit: () => void;
+  onUndo?: () => void;
   value?: string;
+  disabled?: boolean;
+  validate?: (score: number) => boolean;
   className?: string;
 }
 
@@ -16,7 +21,7 @@ interface KeypadProps {
  * change event, and the component will handle invoking its onChange prop by listening for input
  * changes and calling it in response
  */
-const Keypad = ({ onSubmit, onChange, value, className }: KeypadProps) => {
+const Keypad = ({ onSubmit, onChange, onUndo, value, disabled, validate, className }: KeypadProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   // set up change handler internals
@@ -32,16 +37,50 @@ const Keypad = ({ onSubmit, onChange, value, className }: KeypadProps) => {
     inputRef.current!.dispatchEvent(changeEvent);
   };
 
-  const onNumericKeyPress = (e: SyntheticEvent) => {
-    const target = e.target as typeof e.target & { value: string };
-    // validate input here
-    inputRef.current!.value += target.value;
-    dispatchChange();
+  const nextTokenValid = (token: string) => {
+    const currentValue = inputRef.current!.value;
+    const lastToken = currentValue.at(-1);
+
+    if (operators.includes(token)) {
+      if (!currentValue) return false;
+      return !operators.includes(lastToken);
+    } else {
+      return true;
+    }
   };
 
-  const onDelete = () => {
-    inputRef.current!.value = inputRef.current!.value.slice(0, -1);
-    dispatchChange();
+  const scoreValid = (evalString: string) => {
+    if (operators.includes(evalString.at(-1))) return false;
+    const result = validate?.(eval(evalString) ?? 0);
+    return result ?? true;
+    // return validate?.(eval(evalString) ?? 0) ?? true;
+  };
+
+  const partialScoreValid = (evalString: string) => {
+    if (operators.includes(evalString.at(-1))) return true;
+    const result = validate?.(eval(evalString));
+    return result ?? true;
+    // return validate?.(eval(evalString) ?? 0) ?? true;
+  };
+
+  const onNumericKeyPress = (e: SyntheticEvent) => {
+    const { value: token } = e.target as typeof e.target & { value: string };
+    const currentValue = inputRef.current!.value;
+    const newValue = currentValue + token;
+
+    if (nextTokenValid(token) && partialScoreValid(newValue)) {
+      inputRef.current!.value = newValue;
+      dispatchChange();
+    }
+  };
+
+  const handleUndo = () => {
+    if (onUndo) {
+      onUndo();
+    } else {
+      inputRef.current!.value = inputRef.current!.value.slice(0, -1);
+      dispatchChange();
+    }
   };
 
   const onClear = () => {
@@ -49,34 +88,69 @@ const Keypad = ({ onSubmit, onChange, value, className }: KeypadProps) => {
     dispatchChange();
   };
 
+  const handleSubmit = () => {
+    const submitValue = inputRef.current!.value;
+    if (scoreValid(submitValue)) onSubmit();
+  };
+
   return (
-    <div className={className}>
-      <input readOnly value={formatEvalString(value || '')} />
-      <input ref={inputRef} onChange={onChange} value={value} />
-      <div>
-        <button onClick={onNumericKeyPress} value={1} type="button">1</button>
-        <button onClick={onNumericKeyPress} value={2} type="button">2</button>
-        <button onClick={onNumericKeyPress} value={3} type="button">3</button>
+    <div className={`flex flex-col text-xl gap-2 ${className}`}>
+      <input readOnly value={formatEvalString(value || '')} className="text-center py-4 text-2xl" />
+      <input readOnly className="hidden" ref={inputRef} onChange={onChange} value={value} />
+      <div className="flex items-stretch justify-between gap-2 [&>button]:w-full [&>button:focus]:outline-none">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={1} type="button">
+          1
+        </button>
+        <button onClick={onNumericKeyPress} disabled={disabled} value={2} type="button">
+          2
+        </button>
+        <button onClick={onNumericKeyPress} disabled={disabled} value={3} type="button">
+          3
+        </button>
       </div>
-      <div>
-        <button onClick={onNumericKeyPress} value={4} type="button">4</button>
-        <button onClick={onNumericKeyPress} value={5} type="button">5</button>
-        <button onClick={onNumericKeyPress} value={6} type="button">6</button>
+      <div className="flex items-stretch justify-between gap-2 [&>button]:w-full [&>button:focus]:outline-none">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={4} type="button">
+          4
+        </button>
+        <button onClick={onNumericKeyPress} disabled={disabled} value={5} type="button">
+          5
+        </button>
+        <button onClick={onNumericKeyPress} disabled={disabled} value={6} type="button">
+          6
+        </button>
       </div>
-      <div>
-        <button onClick={onNumericKeyPress} value={7} type="button">7</button>
-        <button onClick={onNumericKeyPress} value={8} type="button">8</button>
-        <button onClick={onNumericKeyPress} value={9} type="button">9</button>
+      <div className="flex items-stretch justify-between gap-2 [&>button]:w-full [&>button:focus]:outline-none">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={7} type="button">
+          7
+        </button>
+        <button onClick={onNumericKeyPress} disabled={disabled} value={8} type="button">
+          8
+        </button>
+        <button onClick={onNumericKeyPress} disabled={disabled} value={9} type="button">
+          9
+        </button>
       </div>
-      <div>
-        <button onClick={onNumericKeyPress} value={MATH_MULT} type="button">{MULT}</button>
-        <button onClick={onNumericKeyPress} value={0} type="button">0</button>
-        <button onClick={onNumericKeyPress} value={MATH_PLUS} type="button">{PLUS}</button>
+      <div className="flex items-stretch justify-between gap-2 [&>button]:w-full [&>button:focus]:outline-none">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={MULT_MATH} type="button">
+          {MULT}
+        </button>
+        <button onClick={onNumericKeyPress} disabled={disabled} value={0} type="button">
+          0
+        </button>
+        <button onClick={onNumericKeyPress} disabled={disabled} value={PLUS_MATH} type="button">
+          {PLUS}
+        </button>
       </div>
-      <div>
-        <button onClick={onDelete} type="button">back</button>
-        <button onClick={onClear} type="button">clear</button>
-        <button onClick={onSubmit} type="button">enter</button>
+      <div className="flex items-stretch justify-between gap-2 [&>button]:w-full [&>button:focus]:outline-none">
+        <button onClick={handleUndo} disabled={disabled} type="button">
+          back
+        </button>
+        <button onClick={onClear} disabled={disabled} type="button">
+          clear
+        </button>
+        <button onClick={handleSubmit} disabled={disabled} type="button">
+          enter
+        </button>
       </div>
     </div>
   );
