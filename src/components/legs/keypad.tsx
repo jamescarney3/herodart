769fc2590183f35e310e91604316a284e7b42f5f
James Carney <jmcarneyiii@gmail.com>
2025-10-27 12:51:40 -0400

    HD-14 add initial legs keypad and tests

--- src/components/legs/keypad.tsx
+++ src/components/legs/keypad.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useRef } from 'react';
-import type { ChangeEvent, SyntheticEvent, ChangeEventHandler, MouseEventHandler } from 'react';
+import type { ChangeEvent, SyntheticEvent, ChangeEventHandler } from 'react';
 import { formatEvalString, MULT, PLUS, MULT_MATH, PLUS_MATH } from '~/lib/utils';
+
+const operators: Array<string | undefined> = [MULT_MATH, PLUS_MATH];
 
 interface KeypadProps {
   onChange: ChangeEventHandler<HTMLInputElement>;
-  onSubmit: MouseEventHandler<HTMLButtonElement>;
+  onSubmit: () => void;
   onUndo?: () => void;
   value?: string;
   disabled?: boolean;
+  validate?: (score: number) => boolean;
   className?: string;
   // name: bring back for a11y
   // onUndo?: override clear action if the parent component says so
@@ -23,7 +26,7 @@ interface KeypadProps {
  * change event, and the component will handle invoking its onChange prop by listening for input
  * changes and calling it in response
  */
-const Keypad = ({ onSubmit, onChange, onUndo, value, className }: KeypadProps) => {
+const Keypad = ({ onSubmit, onChange, onUndo, value, disabled, validate, className }: KeypadProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   // set up change handler internals
@@ -51,6 +54,31 @@ const Keypad = ({ onSubmit, onChange, onUndo, value, className }: KeypadProps) =
     }
   };
 
+  const scoreValid = (evalString: string) => {
+    if (operators.includes(evalString.at(-1))) return false;
+    const result = validate?.(eval(evalString) ?? 0);
+    return result ?? true;
+    // return validate?.(eval(evalString) ?? 0) ?? true;
+  };
+
+  const partialScoreValid = (evalString: string) => {
+    if (operators.includes(evalString.at(-1))) return true;
+    const result = validate?.(eval(evalString));
+    return result ?? true;
+    // return validate?.(eval(evalString) ?? 0) ?? true;
+  };
+
+  const onNumericKeyPress = (e: SyntheticEvent) => {
+    const { value: token } = e.target as typeof e.target & { value: string };
+    const currentValue = inputRef.current!.value;
+    const newValue = currentValue + token;
+
+    if (nextTokenValid(token) && partialScoreValid(newValue)) {
+      inputRef.current!.value = newValue;
+      dispatchChange();
+    }
+  };
+
   const handleUndo = () => {
     if (onUndo) {
       onUndo();
@@ -74,59 +102,59 @@ const Keypad = ({ onSubmit, onChange, onUndo, value, className }: KeypadProps) =
   return (
     <div className={`flex flex-col text-xl gap-2 ${className}`}>
       <input readOnly value={formatEvalString(value || '')} className="text-center py-4 text-2xl" />
-      <input className="hidden" ref={inputRef} onChange={onChange} value={value} />
+      <input readOnly className="hidden" ref={inputRef} onChange={onChange} value={value} />
       <div className="flex items-stretch justify-between gap-2 [&>button]:w-full [&>button:focus]:outline-none">
-        <button onClick={onNumericKeyPress} value={1} type="button">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={1} type="button">
           1
         </button>
-        <button onClick={onNumericKeyPress} value={2} type="button">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={2} type="button">
           2
         </button>
-        <button onClick={onNumericKeyPress} value={3} type="button">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={3} type="button">
           3
         </button>
       </div>
       <div className="flex items-stretch justify-between gap-2 [&>button]:w-full [&>button:focus]:outline-none">
-        <button onClick={onNumericKeyPress} value={4} type="button">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={4} type="button">
           4
         </button>
-        <button onClick={onNumericKeyPress} value={5} type="button">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={5} type="button">
           5
         </button>
-        <button onClick={onNumericKeyPress} value={6} type="button">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={6} type="button">
           6
         </button>
       </div>
       <div className="flex items-stretch justify-between gap-2 [&>button]:w-full [&>button:focus]:outline-none">
-        <button onClick={onNumericKeyPress} value={7} type="button">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={7} type="button">
           7
         </button>
-        <button onClick={onNumericKeyPress} value={8} type="button">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={8} type="button">
           8
         </button>
-        <button onClick={onNumericKeyPress} value={9} type="button">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={9} type="button">
           9
         </button>
       </div>
       <div className="flex items-stretch justify-between gap-2 [&>button]:w-full [&>button:focus]:outline-none">
-        <button onClick={onNumericKeyPress} value={MULT_MATH} type="button">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={MULT_MATH} type="button">
           {MULT}
         </button>
-        <button onClick={onNumericKeyPress} value={0} type="button">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={0} type="button">
           0
         </button>
-        <button onClick={onNumericKeyPress} value={PLUS_MATH} type="button">
+        <button onClick={onNumericKeyPress} disabled={disabled} value={PLUS_MATH} type="button">
           {PLUS}
         </button>
       </div>
       <div className="flex items-stretch justify-between gap-2 [&>button]:w-full [&>button:focus]:outline-none">
-        <button onClick={handleUndo} type="button">
+        <button onClick={handleUndo} disabled={disabled} type="button">
           back
         </button>
-        <button onClick={onClear} type="button">
+        <button onClick={onClear} disabled={disabled} type="button">
           clear
         </button>
-        <button onClick={onSubmit} type="button">
+        <button onClick={handleSubmit} disabled={disabled} type="button">
           enter
         </button>
       </div>
--- test/components/legs/keypad.test.tsx
+++ test/components/legs/keypad.test.tsx
@@ -48,8 +48,37 @@ describe('Keypad', () => {
     const { getByText } = render(<Keypad onChange={handleChange} onSubmit={() => {}} value="2" />);
     fireEvent.click(getByText('×'));
     expect(handleChange).toHaveBeenCalled();
+  });
+
+  it('does not call change handler if input is invalid for eval', () => {
+    const handleChange = vi.fn();
+    const { getByText } = render(<Keypad onChange={handleChange} onSubmit={() => {}} value="2" />);
+    fireEvent.click(getByText('×'));
     fireEvent.click(getByText('+'));
-    expect(handleChange).toHaveBeenCalledTimes(2);
+    expect(handleChange).toHaveBeenCalledTimes(1);
+  });
+
+  it('conditionally validates input', () => {
+    const onChange = vi.fn();
+    const onSubmit = vi.fn();
+    const validate = (value) => value < 10;
+    const { getByText } = render(
+      <Keypad onChange={onChange} onSubmit={onSubmit} validate={validate} value="" />
+    );
+    fireEvent.click(getByText('enter'));
+    expect(onSubmit).toHaveBeenCalled();
+    fireEvent.click(getByText('+'));
+    expect(onChange).not.toHaveBeenCalled();
+    fireEvent.click(getByText('2'));
+    expect(onChange).toHaveBeenCalled();
+    fireEvent.click(getByText('+'));
+    fireEvent.click(getByText('enter'));
+    expect(onChange).toHaveBeenCalledTimes(2);
+    expect(onSubmit).toHaveBeenCalledTimes(1);
+    fireEvent.click(getByText('2'));
+    fireEvent.click(getByText('enter'));
+    expect(onChange).toHaveBeenCalledTimes(3);
+    expect(onSubmit).toHaveBeenCalled(2);
   });
 
   it('deletes last character on back', () => {
@@ -57,6 +86,13 @@ describe('Keypad', () => {
     const { getByText } = render(<Keypad onChange={handleChange} onSubmit={() => {}} value="123" />);
     fireEvent.click(getByText('back'));
     expect(handleChange).toHaveBeenCalled();
+  });
+
+  it('conditionally calls onUndo on undo', () => {
+    const onUndo = vi.fn();
+    const { getByText } = render(<Keypad onUndo={onUndo} value="123" />);
+    fireEvent.click(getByText('back'));
+    expect(onUndo).toHaveBeenCalled();
   });
 
   it('clears input on clear', () => {
