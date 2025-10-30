d33eb96b1fa6017200e2192f3c72335501f40594
James Carney <jmcarneyiii@gmail.com>
2025-10-30 04:57:26 -0400

    HD-27 add initial legs game report component

--- src/components/legs/index.ts
+++ src/components/legs/index.ts
@@ -1,2 +1,3 @@
 export { default as Game } from '~/components/legs/game';
 export { default as Keypad } from '~/components/legs/keypad';
+export { default as LegsReport } from '~/components/legs/report';
