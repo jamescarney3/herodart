4a26e901db1fe923772e85266698c3587fb9d0be
James Carney <jmcarneyiii@gmail.com>
2025-11-03 15:04:12 -0500

    HD-29 add legs score toast component

---
+++ src/components/legs/index.ts
@@ -0,0 +1,4 @@
+export { default as Game } from '~/components/legs/game';
+export { default as Keypad } from '~/components/legs/keypad';
+export { default as LegsReport } from '~/components/legs/report';
+export { default as ScoreToast } from '~/components/legs/score-toast';
