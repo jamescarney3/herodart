47e308cf7a913af2d17293434ed1f71c78db2c9b
James Carney <jmcarneyiii@gmail.com>
2025-11-03 02:22:05 -0500

    HD-27 install uuid and add new game creation/tests to legs game hook

--- src/hooks/use-legs-game.ts
+++ src/hooks/use-legs-game.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState, useRef } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import Observer from '~/lib/v2/observer';
 import LegsGame from '~/lib/legs/legs-game';
@@ -8,8 +9,6 @@ const useLegsGame = () => {
   const gameRef = useRef<LegsGame | null>(null);
 
   useEffect(() => {
-    // id probably becomes a uuid or deserialized from some arbitrary kind of storage eventually
-    const id = 'main';
     // compiler sees create invoked as *always* returning a Model instance even though it never
     // actually can - need to find a way to type this at the model level so it can know what its
     // actually getting when that method gets called
@@ -20,8 +19,22 @@ const useLegsGame = () => {
     forceUpdate(() => new Object());
   }, [forceUpdate, gameRef]);
 
+  const newGame = () => {
+    // cleanup all legs-related model instances until there's a good serialization and
+    // storage strategy for them; likely localStorage initially if not server side
+    const game = gameRef.current;
+    if (game) {
+      const { players, rounds } = game;
+      game.delete();
+      players!.forEach((player) => player.delete());
+      rounds!.forEach((round) => round.delete());
+    }
 
-  return { game: gameRef.current };
+    gameRef.current = <LegsGame>LegsGame.create({ id: uuidv4() });
+    forceUpdate(() => new Object());
+  };
+
+  return { game: gameRef.current, newGame };
 };
 
 export default useLegsGame;
