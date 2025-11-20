0eaf3f8fd19a53ef85898a17f37684852ce6c030
James Carney <jmcarneyiii@gmail.com>
2025-11-20 15:44:41 -0500

    HD-42 add shanghai marks component and tests

--- src/components/shanghai/index.ts
+++ src/components/shanghai/index.ts
@@ -1,2 +1,3 @@
 export { default as ShanghaiReport } from '~/components/shanghai/shanghai-report';
 export { default as FlightIcon } from '~/components/shanghai/flight-icon';
+export { default as ShanghaiMarks } from '~/components/shanghai/shanghai-marks';
