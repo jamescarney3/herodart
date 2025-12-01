3a52b15eeae0a5ff1dbe4d8288585111da364b22
James Carney <jmcarneyiii@gmail.com>
2025-12-01 12:13:12 -0500

    HD-36 move legs to page

--- test/pages/legs-game.test.tsx
+++ test/pages/legs-game.test.tsx
@@ -1,29 +1,29 @@
 import { describe, it, expect } from 'vitest';
 import { render } from '@testing-library/react';
 
-import { Game } from '~/components/legs';
+import LegsGame from '~/pages/legs-game';
 
 describe('LegsGame', () => {
   it('renders without game', () => {
-    const { container } = render(<Game />);
+    const { container } = render(<LegsGame />);
     expect(container).to.exist;
   });
 
   it('renders with game in setup phase', () => {
     const game = { started: false };
-    const { container } = render(<Game game={game} />);
+    const { container } = render(<LegsGame game={game} />);
     expect(container).to.exist;
   });
 
   it('renders with game in progress', () => {
     const game = { started: true, finished: false };
-    const { container } = render(<Game game={game} />);
+    const { container } = render(<LegsGame game={game} />);
     expect(container).to.exist;
   });
 
   it('renders with finished game', () => {
     const game = { started: true, finished: true };
-    const { container } = render(<Game game={game} />);
+    const { container } = render(<LegsGame game={game} />);
     expect(container).to.exist;
   });
 });
