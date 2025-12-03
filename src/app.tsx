b033d66462b394bb2e373a6ea8ee6cd95d47ac93
James Carney <jmcarneyiii@gmail.com>
2025-12-03 14:16:36 -0500

    P7E try standalone checks in app module

--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 
 import { LegsGame } from '~/pages';
 
+const standaloneChecks = [
+  !!(window.navigator as typeof window.navigator & { standalone: boolean })?.standalone,
+  window.matchMedia('(display-mode: standalone)').matches,
+];
+
 function App() {
   return (
     <BrowserRouter basename="herodart">
+      <div className="fixed text-red-500 bg-gray-100 bg-opacity-50 text-xl">
+        <div>navigator: {String(standaloneChecks[0])}</div>
+        <div>matchMedia: {String(standaloneChecks[1])}</div>
+      </div>
       <Routes>
         <Route path="*" element={<LegsGame />} />
       </Routes>
