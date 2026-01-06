5a1d02901042c5598f8329becfa6f9d328e681a4
James Carney <jmcarneyiii@gmail.com>
2026-01-05 23:09:26 -0500

    HD-53 gather+export game menu component

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,3 +1,4 @@
 export { default as Keypad } from '~/components/shared/keypad';
 export { default as InstallModal } from '~/components/shared/install-modal';
 export { default as MenuButton } from '~/components/shared/menu-button';
+export { default as GameMenuModal } from '~/components/shared/game-menu-modal';
