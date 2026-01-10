264fed6893168387e29b85aee937ebec7e1b9887
James Carney <jmcarneyiii@gmail.com>
2026-01-09 20:16:02 -0500

    HD-56 add ko-fi link component

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,3 +2,4 @@ export { default as Keypad } from '~/components/shared/keypad';
 export { default as InstallModal } from '~/components/shared/install-modal';
 export { default as MenuButton } from '~/components/shared/menu-button';
 export { default as GameMenuModal } from '~/components/shared/game-menu-modal';
+export { default as KofiLink } from '~/components/shared/kofi-link';
