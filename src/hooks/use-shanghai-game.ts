27489c9055b16314a081ac69ab9356d00ae4e025
James Carney <jmcarneyiii@gmail.com>
2026-04-12 14:12:36 -0400

    HD-60 update game hooks

--- src/hooks/use-shanghai-game.ts
+++ src/hooks/use-shanghai-game.ts
@@ -2,24 +2,36 @@ import { useEffect, useState, useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import { Observer } from '@jamescarney3/microrm';
-import ShanghaiGame from '~/lib/shanghai/shanghai-game';
+import { ShanghaiGame, ShanghaiRules } from '~/lib/shanghai';
 
 // NB: see use-legs-game hook for more detailed notes
+
+const initShanghaiGame = (): ShanghaiGame => {
+  const rules = ShanghaiRules.create({ id: uuidv4(), started: false });
+  const game = ShanghaiGame.create({ id: uuidv4(), rules });
+  return game as ShanghaiGame;
+};
+
+const cleanupShanghaiGame = (game: ShanghaiGame): void => {
+  game.rounds.forEach((round) => round.delete());
+  game.players.forEach((player) => player.delete());
+  game.delete();
+};
 
 const useLegsGame = () => {
   const [, forceUpdate] = useState<unknown>(new Object());
   const gameRef = useRef<ShanghaiGame | null>(null);
 
   useEffect(() => {
-    gameRef.current ??= <ShanghaiGame>ShanghaiGame.create({ id: uuidv4() });
+    gameRef.current ??= initShanghaiGame();
 
     Observer.subscribe(() => forceUpdate(() => new Object()));
     forceUpdate(() => new Object());
   }, [forceUpdate, gameRef]);
 
   const newGame = () => {
-    // TODO: consider cleaning up after the current game if it exists
-    gameRef.current = <ShanghaiGame>ShanghaiGame.create({ id: uuidv4() });
+    cleanupShanghaiGame(gameRef.current);
+    gameRef.current = initShanghaiGame();
     forceUpdate(() => new Object());
   };
 
