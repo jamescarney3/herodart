31794ee151753617f3e7fb41cfe5f346791c4f7d
James Carney <jmcarneyiii@gmail.com>
2026-05-28 14:40:54 -0400

    HD-68 purge old menu components

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,7 +1,5 @@
 export { default as ContextMenu } from '~/components/shared/context-menu';
 export { default as Keypad } from '~/components/shared/keypad';
 export { default as InstallModal } from '~/components/shared/install-modal';
-export { default as MenuButton } from '~/components/shared/menu-button';
-export { default as GameMenuModal } from '~/components/shared/game-menu-modal';
 export { default as KofiLink } from '~/components/shared/kofi-link';
 export { default as RadioGroup } from '~/components/shared/radio-group';
