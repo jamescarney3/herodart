52f15e1d1470cb25a3cf0026681661eaaa39bdb0
James Carney <jmcarneyiii@gmail.com>
2026-08-19 17:53:01 -0400

    R6R fix container top padding for context menu

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react';
 
 // NB: not sure if this is the best name for this thing, soemthing like <Main /> might be better?
 const Container = ({ children }: { children: ReactNode }) => {
-  return <main className="h-screen flex flex-col gap-2 p-2">{children}</main>;
+  return <main className="h-screen flex flex-col gap-2 p-2 pt-10">{children}</main>;
 };
 
 export default Container;
